f05538da79bbb10d4edef0d0e791c4135044d6ba
ariskha <145279156+ariskha@users.noreply.github.com>
2023-12-03 15:19:03 +0700

    Update Petugas.php

--- application/models/Petugas.php
+++ application/models/Petugas.php
@@ -4,30 +4,39 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Petugas extends CI_Model {
 
 	function get_petugas(){
+		//petugas//
 		$result=$this->db->query("SELECT * FROM petugas");
         return $result;
 	}
 
 	function get_petugas_by_id($id){
+		//memuat petugas//
 		$result=$this->db->query("SELECT * FROM petugas WHERE kode_petugas='$id'");
         return $result;
 	}
+	
 	function get_login($username,$password){
+		//memuat login//
     	$result=$this->db->query("SELECT * FROM petugas WHERE username='$username' && password='$password' ");
         return $result;	
     }
+	
     function get_max(){ 
 	        $result=$this->db->query("SELECT max(kode_petugas) as kode_max FROM petugas");
 	        return $result;
     }
 	function set_petugas($kode_petugas="",$nama_petugas="",$alamat_petugas="",$username="",$password="",$level=""){
+		//membuat data petugas//
 		$result=$this->db->query("INSERT INTO petugas VALUES('$kode_petugas','$nama_petugas','$alamat_petugas','$username','$password','$level')");
         return $result;
 	}
+	
 	function update_petugas($kode_petugas="",$nama_petugas="",$alamat_petugas="",$username="",$password="",$level=""){
+		//mengupdate data petugas//
 		$result=$this->db->query("UPDATE petugas SET nama_petugas='$nama_petugas',alamat_petugas='$alamat_petugas',username='$username',password='$password',level='$level' WHERE kode_petugas='$kode_petugas'");
         return $result;
 	}
+	
 	function update_petugas_no_password($kode_petugas="",$nama_petugas="",$alamat_petugas="",$username="",$level=""){
 		$result=$this->db->query("UPDATE petugas SET nama_petugas='$nama_petugas',alamat_petugas='$alamat_petugas',username='$username',level='$level' WHERE kode_petugas='$kode_petugas'");
         return $result;
@@ -37,6 +46,7 @@ class Petugas extends CI_Model {
 	        return $result;
 	}
 	function delete_petugas($id=""){
+		//menghapus data petugas//
 		$result=$this->db->query("DELETE FROM petugas WHERE kode_petugas='$id'");
         return $result;
 	}
